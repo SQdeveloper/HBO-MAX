03c09ca62cdb901cdc75425cf18bb84956ec9992
Jefferson <jefferson.silvaquinto@gmail.com>
2023-02-22 16:05:00 -0500

    agregando responsividad

--- code/scripts/preguntas.js
+++ code/scripts/preguntas.js
@@ -5,13 +5,32 @@ let flag = [true, true, true, true, true];
 
 buttonPlus.forEach((bt, index)=>{
     bt.addEventListener("click", ()=>{
-        parrafo[index].classList.toggle("active");
+        /*****Iniabilitamos las demas preguntas ******/
+        parrafo.forEach(pr=>{
+            pr.classList.remove("active");
+        });
+        /*****Cambiamos de imagen a la preguntas excepto al que se le hizo click*/
+        imgs.forEach((im, i)=>{
+            if(i != index) {
+                im.src = "./rg/svg/plus.svg";
+            }
+        });
+        /******* Cambiamos los valores de los flag excepto al que se le hizo click********/
+        flag.forEach((fg, i)=>{
+            if(i != index) {
+                flag[i] = true;
+            }
+        });
+
+        /*********** Camibiamos de imagen segun corresponda ********** */
         if(flag[index] === true) {
             imgs[index].src = "./rg/svg/dash.svg";
             flag[index] = false;
+            parrafo[index].classList.add("active");
         }else {
             imgs[index].src = "./rg/svg/plus.svg"
             flag[index] = true;
+            parrafo[index].classList.remove("active");
         }
     });
 });
